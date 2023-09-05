d37e85066e36e0633a9ed869a80e1b8c0d2704e5
franciscalorraynes <franciscalorrayne5@gmail.com>
2023-09-05 15:41:48 -0300

    excluidos > melhor organizacao

--- EventoAcademico/DemoEvento.java
+++
@@ -1,9 +0,0 @@
-package EventoAcademico;
-
-public class DemoEvento {
-    public static void main(String[] args) {
-        System.out.println("\nInformações do evento:");
-        EventoAcademico evento = new EventoAcademico("ECOP", "UFERSA, pdf", 500);
-        // evento.mostraEvento();
-    }
-}
